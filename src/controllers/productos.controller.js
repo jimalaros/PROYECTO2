97b819b611e1c91b0684cb95cdd8ed67232dc10f
jimalaros <jimalaros25@gmail.com>
2021-08-10 12:05:01 -0500

    10 de agosto

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -77,7 +77,6 @@ export const ActualizarProductos = async (req, res) => {
         console.log(error);
         return res.status(500).json(error);
     }
-    
 };
   
 export const EliminarProductos = async (req, res) => {
