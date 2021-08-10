70eb566c4b5d4109ec37c84ceb43cc7dab51289a
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-08-10 11:47:23 -0500

    Todo sobre medios de pago

--- src/controllers/MediodePago.controller.js
+++ src/controllers/MediodePago.controller.js
@@ -9,10 +9,11 @@ export const MediosdePagoxDefecto = async (req,res) => {
         
         // Medios depago por defecto
         const mediosdepago  = await Promise.all([
-            new Producto ({ nombre: "Efectivo"}).save(),
-            new Producto ({ nombre: "Tarjeta de credito"}).save(),
-            new Producto ({ nombre: "Datafono"}).save(),
-            new Producto ({ nombre: "Nequi"}).save()
+            new MediodePago ({ nombre: "Efectivo"}).save(),
+            new MediodePago ({ nombre: "Tarjeta de credito"}).save(),
+            new MediodePago ({ nombre: "Datafono"}).save(),
+            new MediodePago ({ nombre: "Nequi"}).save(),
+            new MediodePago ({ nombre: "PSE"}).save()
         ]);
 
         res.json(mediosdepago);
@@ -25,7 +26,7 @@ export const MediosdePagoxDefecto = async (req,res) => {
 
 export const MediosdePago = async (req,res) => {
     try {
-        const mediosdepago = await Producto.find();
+        const mediosdepago = await MediodePago.find();
         res.json(mediosdepago)
     } catch (error) {
         console.error(error);
@@ -34,24 +35,25 @@ export const MediosdePago = async (req,res) => {
 };
 
 export const CrearMediodePago = async (req, res) => {
-    const { nombre, precio } = req.body;
+    const { nombre } = req.body;
   
     try {
-        const MediodePagoRepetido = await MediodePago.findOne({ nombre });
-        if (MediodePagoRepetido)
+        if(nombre)
         {
-            res.status(400).json('El Medio de pago ya existe');
+            const MediodePagoRepetido = await MediodePago.findOne({ nombre });
+            if (MediodePagoRepetido)
+            {
+                res.status(400).json('El Medio de pago ya existe');
+            }
+            else
+            {
+                const NuevoMediodePago = new MediodePago({ nombre });
+                await NuevoMediodePago.save();
+                
+                res.status(201).json({msg:'Medio de Pago creado con exito'});
+            }
         }
-        else
-        {
-            const NuevoMediodePago = new MediodePago({
-                nombre
-            });
-        
-            const MediodePagoCreado = await NuevoMediodePago.save();
-      
-            res.status(201).json(MediodePagoCreado);
-        }
+        else { res.status(204).json({msg: 'Faltan datos'}) }
     }
     catch (error) {
         console.log(error);
@@ -60,18 +62,32 @@ export const CrearMediodePago = async (req, res) => {
 };
   
 export const ActualizarMediosdePago = async (req, res) => {
-    const updatedMediodePago = await MediodePago.findByIdAndUpdate(req.params.id, req.body,
+    try {
+        const { nombre } = req.body;
+        if(nombre)
         {
-            new: true,
+            const { id } = req.params;
+            const updates = {...req.body};
+            const options = { new: true };
+            await MediodePago.findByIdAndUpdate(id, updates, options);
+            res.status(200).json({msg: 'Medio de Pago editado con exito'});
         }
-    );
-    res.status(204).json(updatedMediodePago);
+        else { res.status(204).json({msg: 'Faltan datos'}) }    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 };
   
 export const EliminarMediosdePago = async (req, res) => {
-    const { id } = req.params;
+    try {
+        const { id } = req.params;
   
-    await MediodePago.findByIdAndDelete(id);
-  
-    res.status(200).json({msg: "El medio de pago fue eliminado con exito"});
+        await MediodePago.findByIdAndDelete(id);
+      
+        res.status(200).json({msg: "El medio de pago fue eliminado con exito"});    
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 };
