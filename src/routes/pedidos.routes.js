ec9f13ee06d7285c2532616e6ed18b1f565a40bf
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-08-16 12:31:05 -0500

    CRUD pedidos

--- src/routes/pedidos.routes.js
+++ src/routes/pedidos.routes.js
@@ -1,16 +1,16 @@
-import { Router } from "express";
+import { Router } from 'express';
+import * as Pedido from '../controllers/pedidos.controller';
+
 const router = Router();
 
-import * as Pedido from "../controllers/pedidos.controller";
+router.get('/', Pedido.Pedidos);
 
-router.get("/", Pedido.Pedidos);
+router.post('/Crear', Pedido.CrearOrden);
 
-router.post("/Crear", Pedido.CrearOrden);
+router.post('/Ordenar/:id', Pedido.Ordenar);
 
-router.post("/Ordenar/:id", Pedido.Ordenar);
+router.put('/Editar/:id', Pedido.ActualizarPedidos);
 
-router.put("/Editar/:id", Pedido.ActualizarPedidos);
-
-router.delete("/Eliminar/:id", Pedido.EliminarPedidos);
+router.delete('/Eliminar/:id', Pedido.EliminarPedidos);
 
 export default router;
