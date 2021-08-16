cf1105c749acc928c6641aaa9572aec5354329f8
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-08-16 12:28:48 -0500

    Testing creación de usuarios

--- src/tests/usuarios.test.js
+++ src/tests/usuarios.test.js
@@ -1,14 +1,46 @@
-import request from "supertest";
-import { app } from "../index.js";
+import request from 'supertest';
+import app from '../index';
 
 /**
- * testing usuarios
+ * testing registro usuarios
  */
 
-it('respond with json contaning a list of all users', done =>{
-    request(app)
-        .get('/usuarios')
-        .set('application/json')
+describe("POST /usuarios/nuevos", () => {  
+    it("respond with 201 created", (done) => {
+      const data = {
+        nombre: "Jaao",
+        apellido: "A",
+        correo: "jaaao@gmail.com",
+        telefono: 311111,
+        direccion: "Hospital",
+        contraseña: "111111",
+        administrador: false,
+      };
+      request(app)
+        .post('/usuarios/nuevos')
+        .send(data)
+        .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
-        .expect(200, done);
+        .expect(201)
+        .expect('"Usuario creado con exito"')
+        .end((err) => {
+          if (err) return done(err);
+          done();
+        });
+    });
+  
+    it("respond with 400 on bad request", (done) => {
+      const data = {};
+      request(app)
+        .post('/usuarios/nuevos')
+        .send(data)
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .expect('"Faltan datos"')
+        .end((err) => {
+          if (err) return done(err);
+          done();
+        });
+    });
 });
