70ff65ce27985258f0a1e1a122e3641640dc7fb1
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-08-16 12:29:49 -0500

    CRUD usuarios

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,15 +1,15 @@
-import { Router } from "express";
+import { Router } from 'express';
+import * as Usuarios from '../controllers/usuarios.controller';
+import { Verificar } from '../middlewares/token.middleware'
+
 const router = Router();
 
-import * as Usuarios from "../controllers/usuarios.controller";
-import { Verificar } from "../middlewares/token.middleware"
+router.get('/', Verificar, Usuarios.Usuarios);
 
-router.get("/", Verificar, Usuarios.Usuarios);
+router.post('/nuevos', Usuarios.CrearUsuario);
 
-router.post("/nuevos", Usuarios.CrearUsuario);
+router.post('/Login', Usuarios.InicioSesion);
 
-router.post("/Login", Usuarios.InicioSesion);
-
-router.delete("/Eliminar/:id", Usuarios.EliminarUsuarios);
+router.delete('/Eliminar/:id', Usuarios.EliminarUsuarios);
 
 export default router;
