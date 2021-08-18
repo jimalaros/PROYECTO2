2b81dd7cc191a4dadc74b8c61446c34b4f50fb9a
jimalaros <48387416+jimalaros@users.noreply.github.com>
2021-08-17 19:12:17 -0500

    test.js

--- src/tests/usuarios.test.js
+++ src/tests/usuarios.test.js
@@ -6,41 +6,40 @@ import app from '../index';
  */
 
 describe("POST /usuarios/nuevos", () => {  
-    it("respond with 201 created", (done) => {
-      const data = {
-        nombre: "Jaao",
-        apellido: "A",
-        correo: "jaaao@gmail.com",
-        telefono: 311111,
-        direccion: "Hospital",
-        contraseña: "111111",
-        administrador: false,
-      };
-      request(app)
-        .post('/usuarios/nuevos')
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(201)
-        .expect('"Usuario creado con exito"')
-        .end((err) => {
-          if (err) return done(err);
-          done();
-        });
-    });
-  
-    it("respond with 400 on bad request", (done) => {
-      const data = {};
-      request(app)
-        .post('/usuarios/nuevos')
-        .send(data)
-        .set('Accept', 'application/json')
-        .expect('Content-Type', /json/)
-        .expect(400)
-        .expect('"Faltan datos"')
-        .end((err) => {
-          if (err) return done(err);
-          done();
-        });
-    });
+  it("respond with 201 created", (done) => {
+    const data = {
+      nombre: "Jaao",
+      apellido: "A",
+      correo: "jaaao@gmail.com",
+      telefono: 311111,
+      direccion: "Hospital",
+      contraseña: "111111"
+    };
+    request(app)
+      .post('/usuarios/nuevos')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(201)
+      .expect('"Usuario creado con exito"')
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
+
+  it("respond with 400 on bad request", (done) => {
+    const data = {};
+    request(app)
+      .post('/usuarios/nuevos')
+      .send(data)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(400)
+      .expect('"Faltan datos"')
+      .end((err) => {
+        if (err) return done(err);
+        done();
+      });
+  });
 });
